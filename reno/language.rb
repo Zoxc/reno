4b7953beec7554b4f702930269212ca7eef33283
Zoxc <zoxc32@gmail.com>
2010-10-02 19:52:03 +0200

    Removed old code.

--- reno/language.rb
+++ reno/language.rb
@@ -1,30 +1,5 @@
 module Reno
 	class Language
-		class DeferedState
-			def initialize(components, state_class)
-				state = components.owner
-				@package = state.package
-				@private = components == state.private
-				@state_class = state_class
-			end
-			
-			def get_components
-				@private ? @package.state.private : @package.state.public
-			end
-			
-			def get
-				components = get_components
-				
-				if components.has_component?(@state_class, false)
-					components.get_component(@state_class)
-				else
-					state = @state_class.new
-					components.use(state)
-					state
-				end
-			end
-		end
-		
 		def self.use_component(package)
 			package.state.set_option File::Extension, @lang_ext
 			@interface_class.new(package)
